07ae6b64239cb5d70dc0912fce25520c4f2e7071
eng <eng@diag.com>
2015-09-20 15:06:32 -0400

    midClass

---
+++ prototyping/src/prototyping/ExtTagStream.java
@@ -0,0 +1,24 @@
+package prototyping;
+
+import java.net.MalformedURLException;
+
+public class ExtTagStream extends ExtTag {
+
+	protected M3u8InputStream inStream;
+	
+	ExtTagStream(PlayList playList, PlayListScanner scanner, String url) throws MalformedURLException{
+		super(playList, scanner);
+		String myUrl = url;
+		if (!Tokens.URL_PATTERN.matcher(url).matches()){
+			// relative url, build complete 
+			myUrl = playList.inStream.GetUrlNoFN() + '/' + url;
+		}
+		inStream = new M3u8InputStream(myUrl, containingList);
+		inStream.Download();
+	}	
+	
+	public static ExtTagStream Clone(PlayList playList, PlayListScanner scanner, String url) throws MalformedURLException{
+		ExtTagStream clone = new ExtTagStream(playList, scanner, url);
+		return clone;
+	}
+}
