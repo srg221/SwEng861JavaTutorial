43c277d68c3037cb8e4355e5a8f5a2926253a196
eng <eng@diag.com>
2015-10-11 20:54:40 -0400

    final3

--- prototyping/src/prototyping/Main.java
+++ prototyping/src/prototyping/Main.java
@@ -1,40 +1,34 @@
 package prototyping;
 import java.io.*;
 
-//import java.net.MalformedURLException;
-
-
+// simple console test program for MediaStream.java
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// M3u8InputStream test;
+
 		try {
 				System.out.println("url?:\n");
 				BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
 				String url = bufferRead.readLine();
+				// for quick testing w/o typing @ console
 				if (url.isEmpty()) 
 				{
 					//url = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8";
 					//url = "ftp://tomcat:Apac-Hee@146.186.90.203/Arris/ipad.m3u8";
 					//url = "http://tomcat:Apac-Hee@146.186.90.203:8080/Arris/ipad.m3u8";
 					//url = "http://146.186.90.203:8080/Arris/ipad.m3u8";
-					url = "http://localhost/Arris/ipad.m3u8";	
+					//url = "http://localhost/Arris/ipad.m3u8";	
 					//url = "http://127.0.0.1/Arris/ipad.m3u8";
 				}
-/*
-				//test = new M3u8InputStream(url, "C:\\temp");
-				test = new M3u8InputStream(url);
-				test.Download();
-				InputStream iStream = test.GetInputStream(); 
-*/
+				// create an instance of the MediaStream validator
 				MediaStream testMediaStream = new MediaStream(url);
+				// validate it
 				testMediaStream.Validate();
 				
 			} 
 		catch (Exception e) 
 		{
-			// TODO Auto-generated catch block
+			// for debug only, all exceptions should be caught in app
 			e.printStackTrace();
 		}
 	}
